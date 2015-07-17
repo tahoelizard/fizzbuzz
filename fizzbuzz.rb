f7bcc1ca1cff7a6092461217cd7d9131ee93f4b3
Liz <tahoelizard>
2015-07-17 16:39:33 -0700

    changed looping structure to adhere to ruby style guide

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,12 @@
 
-$i = 1
-$num = 100
-
-until $i > $num  do
-	if $i% 3 ==0 && $i% 5 ==0
+(1..100).each do |i|
+	if i% 3 ==0 && i% 5 ==0
 		puts ("fizzbuzz")
-	elsif $i% 3 ==0
+	elsif i% 3 ==0
 		puts ("fizz")
-	elsif $i% 5 == 0 
+	elsif i% 5 == 0 
 		puts ("buzz")
 	else
-		puts($i)
+		puts(i)
 	end
-   $i +=1;
 end
